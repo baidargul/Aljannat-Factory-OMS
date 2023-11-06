fc4470cdf4e7b4423e8174120f7fb33a500f23aa
baidargul <123100022+baidargul@users.noreply.github.com>
2023-11-06 19:22:57 +0500

    Everything working on this step for now

--- app/file/process/[id]/page.tsx
+++ app/file/process/[id]/page.tsx
@@ -1,14 +1,15 @@
 import { Loader } from 'lucide-react'
 import prisma from '@/lib/prisma'
 import React from 'react'
-import fs from 'fs';
-import path from 'path';
 import * as XLSX from 'xlsx';
+import { v4 } from 'uuid';
+import { redirect } from 'next/navigation'
 type Props = {
     params: any
+    req: any
 }
 
-const page = async (props: Props) => {
+async function page(props: Props, context: any) {
 
     const { id } = props.params
 
@@ -18,6 +19,7 @@ const page = async (props: Props) => {
         }
     })
 
+    let isComplete = false
     if (processFile) {
         try {
             const fileContentBuffer = Buffer.from(processFile.content, 'base64');
@@ -26,21 +28,75 @@ const page = async (props: Props) => {
             const sheetName = workbook.SheetNames[0];
             const worksheet = workbook.Sheets[sheetName];
             const fileData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+            const tableHeaders: any = fileData[0]
             const data = fileData
-            // console.log(data)
 
-            data.map((row: any, index: number) => {
+            data.map(async (row: any, index: number) => {
                 if (index === 0) return
+                let rowObject: any = {
+                    id: v4(),
+                }
+                const fetchedCustomer = await getCustomer(row, tableHeaders)
                 row.map((cell: any, index: number) => {
-                    if (index === 0) console.log(`Celld:`, excelSerialToDate(cell))
-                    console.log(`cell:`, cell)
+                    if (tableHeaders[index] === "Booking") {
+                        rowObject = {
+                            ...rowObject,
+                            [tableHeaders[index]]: excelSerialToDate(cell)
+                        }
+                        return
+                    }
+                    rowObject = {
+                        ...rowObject,
+                        [tableHeaders[index]]: cell
+                    }
+                })
+                await prisma.orders.create({
+                    data: {
+                        id: rowObject.id,
+                        dateOfBooking: rowObject.Booking,
+                        status: rowObject.Status,
+                        note: rowObject.Note,
+                        confirmedBy: rowObject.Confirmed,
+                        product: rowObject.Product,
+                        variant: rowObject.Variant,
+                        weight: rowObject.Weight,
+                        amount: rowObject.Amount,
+                        courier: rowObject.Courier,
+                        trackingNo: String(rowObject.Tracking),
+                        customerId: fetchedCustomer.id
+                    }
                 })
             })
 
+            await prisma.sheets.delete({
+                where: {
+                    id: id
+                }
+            })
+
+            isComplete = true
 
         } catch (error) {
             console.error('Error processing the file:', error);
+            redirectToErrorPage()
         }
+    }
+
+    if (!processFile) {
+        redirectToErrorPage()
+    }
+
+
+    if(isComplete) {
+        redirectToCompletePage()
+    }
+
+    function redirectToCompletePage() {
+        redirect('/file/process/complete')
+    }
+
+    function redirectToErrorPage() {
+        redirect('/file/process/error')
     }
 
     return (
@@ -67,13 +123,57 @@ function excelSerialToDate(serial: any) {
     const date = new Date(Date.UTC(1899, 11, 30)); // Excel's starting date is one day ahead (December 30, 1899)
 
     date.setUTCDate(date.getUTCDate() + serial);
-    return toLocalDateAndTimeFormat(date);
+    return date;
 }
 
-function toLocalDateAndTimeFormat(date: any, time: boolean = false) {
+function toLocalDateAndTimeFormat(date: any, time: boolean = false, systemFormat: boolean = false) {
+    if (systemFormat) {
+        return date
+    }
     const localDate = new Date(date);
     if (time) {
         return localDate.toLocaleDateString() + ' ' + localDate.toLocaleTimeString();
     }
     return localDate.toLocaleDateString();
+}
+
+async function getCustomer(row: any, tableHeaders: any) {
+    let customer: any = {}
+    row.map((cell: any, index: number) => {
+        if (tableHeaders[index] === "Customer") {
+            customer.name = cell
+        }
+        if (tableHeaders[index] === "Phone") {
+            customer.phone = cell
+        }
+        if (tableHeaders[index] === "City") {
+            customer.city = cell
+        }
+        if (tableHeaders[index] === "Address") {
+            customer.address = cell
+        }
+    })
+
+    console.log(`target phone: `, customer.phone)
+    const fetchedCustomer = await prisma.customers.findFirst({
+        where: {
+            phone: customer.phone
+        }
+    })
+
+    if (fetchedCustomer) {
+        return fetchedCustomer
+    }
+
+    const createdCustomer = await prisma.customers.create({
+        data: {
+            id: v4(),
+            name: customer.name,
+            phone: customer.phone,
+            city: customer.city,
+            address: customer.address
+        }
+    })
+
+    return createdCustomer
 }
---
+++ app/file/process/error/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { FileWarning } from 'lucide-react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+type Props = {}
+
+const ProcessError = (props: Props) => {
+    const router = useRouter()
+
+
+    function handleRedirect() {
+        setTimeout(() => {
+            router.push('/')
+        }, 2000)
+    }
+
+    handleRedirect()
+
+    return (
+        <div className='w-full min-h-screen bg-slate-300 flex justify-center items-center select-none '>
+
+            <div className='rounded-md p-4 bg-slate-800 text-white'>
+                <div className='flex flex-col gap-2 items-center '>
+                    <p className='text-red-300'>
+                        <FileWarning size={22} />
+                    </p>
+                    <p className='font-semibold'>
+                        Invalid File
+                    </p>
+                </div>
+                <p className='text-center text-xs'>
+                    Redirecting to Homepage...
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProcessError
