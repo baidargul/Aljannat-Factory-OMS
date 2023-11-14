4e8b1f3632ef8f4b0ef46bd9b6f18e8b87461f0b
Baidar Gul <123100022+baidargul@users.noreply.github.com>
2023-11-14 17:25:36 +0500

    added textbox as input and disable number up down keys

--- app/(create)/create/order/page.tsx
+++ app/(create)/create/order/page.tsx
@@ -1,3 +1,4 @@
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 type Props = {}
@@ -6,8 +7,15 @@ const page = (props: Props) => {
     return (
         <div className=''>
 
-            <div className='bg-slate-300 rounded-md border drop-shadow-md'>
-                
+            <div className='bg-slate-300 rounded-md border drop-shadow-md p-2 select-none'>
+                <div>
+                    <h1 className='text-lg font-semibold'>
+                        Create order
+                    </h1>
+                    <section>
+                        
+                    </section>
+                </div>
             </div>
 
         </div>
