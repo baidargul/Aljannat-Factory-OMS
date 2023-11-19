91d94438809e139afcd5c191f3e0140e659ac888
Baidar Gul <123100022+baidargul@users.noreply.github.com>
2023-11-20 03:04:54 +0500

    added header to grid

--- components/Grid/GenericRowsHolder.tsx
+++ components/Grid/GenericRowsHolder.tsx
@@ -11,17 +11,30 @@ const GenericRowsHolder = (props: Props) => {
 
 
     return (
-        <ScrollArea className='w-full h-[550px]'>
-            <div className=''>
-                {
-                    props.orders.length > 0 && props.orders.map((row: any,index:number) => (
-                        <div key={row.id} className=''>
-                            <GenericRow row={row} index={index} />
-                        </div>
-                    ))
-                }
+        <div>
+            <div className='grid grid-cols-9 tracking-wide text-sm justify-items-start bg-red-800 text-white border-red-800 border p-2'>
+                <div>#</div>
+                <div>Job Created</div>
+                <div>Customer</div>
+                <div>Product</div>
+                <div>Weight</div>
+                <div>Variation</div>
+                <div>Bill</div>
+                <div>Status</div>
+                <div>Courier#</div>
             </div>
-        </ScrollArea>
+            <ScrollArea className='w-full h-[550px]'>
+                <div className=''>
+                    {
+                        props.orders.length > 0 && props.orders.map((row: any, index: number) => (
+                            <div key={row.id} className=''>
+                                <GenericRow row={row} index={index} />
+                            </div>
+                        ))
+                    }
+                </div>
+            </ScrollArea>
+        </div>
     )
 }
 
