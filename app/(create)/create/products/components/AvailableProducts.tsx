40d3277c73d872523d49c1d1f7fb4be2b1f765f3
Baidar Gul <baidargul@outlook.com>
2023-12-02 22:53:36 +0500

    better UI

--- app/(create)/create/products/components/AvailableProducts.tsx
+++ app/(create)/create/products/components/AvailableProducts.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import prisma from '@/lib/prisma'
-import { product } from '@prisma/client'
+import { product, productVariations } from '@prisma/client'
 import AvailableProductSelector from './subComponents/AvailableProductSelector'
 import Image from 'next/image'
+import { ScrollArea } from '@/components/ui/scroll-area'
 type Props = {}
+
 
 const AvailableProducts = async (props: Props) => {
 
     const availableProducts = await prisma.product.findMany({
+        include: {
+            productVariations: true
+        },
         orderBy: {
             name: 'asc'
         }
@@ -17,26 +22,34 @@ const AvailableProducts = async (props: Props) => {
 
     return (
         <div>
-            <div className='grid grid-cols-3 gap-2'>
-                {
-                    availableProducts.map((product: product) => {
-                        return (
-                            <div key={product.id}>
-                                <AvailableProductSelector product={product}>
-                                    <div className='flex gap-1 items-center'>
-                                        <div>
-                                            <Image src={`https://getpcsofts.info/wp-content/uploads/2023/09/JetBrains-DataGrip-Crack-300x202.png`} className='w-16 h-16' width={32} height={32} alt='image' />
+            <ScrollArea className='h-[200px]' type='always'>
+                <div className='grid grid-cols-3 gap-2'>
+                    {
+                        availableProducts.map((product: any) => {
+                            const productVariations: productVariations[] = product.productVariations
+                            return (
+                                <div key={product.id}>
+                                    <AvailableProductSelector product={product}>
+                                        <div className='flex gap-1 items-center'>
+                                            <div>
+                                                <Image src={`https://getpcsofts.info/wp-content/uploads/2023/09/JetBrains-DataGrip-Crack-300x202.png`} className='w-16 h-16' width={32} height={32} alt='image' />
+                                            </div>
+                                            <div className='flex gap-1 items-center'>
+                                                <div>
+                                                    {product.name}
+                                                </div>
+                                                <div className={`text-xs text-start w-fit scale-75 p-1 bg-slate-50 border border-slate-300 rounded-md ${productVariations.length < 1 && "hidden"}`}>
+                                                    {productVariations.length}
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div>
-                                            {product.name}
-                                        </div>
-                                    </div>
-                                </AvailableProductSelector>
-                            </div>
-                        )
-                    })
-                }
-            </div>
+                                    </AvailableProductSelector>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </ScrollArea>
         </div>
     )
 }
