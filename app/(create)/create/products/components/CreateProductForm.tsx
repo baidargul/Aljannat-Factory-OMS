3546cd9393966b1f0fd65e0d4f2c36a640c139d6
Baidar Gul <baidargul@outlook.com>
2023-12-02 21:11:35 +0500

    added Image input dummy

--- app/(create)/create/products/components/CreateProductForm.tsx
+++ app/(create)/create/products/components/CreateProductForm.tsx
@@ -9,6 +9,7 @@ type Props = {}
 
 const CreateProductForm = (props: Props) => {
     const [productName, setProductName] = useState('')
+    const [imageUrl, setImageUrl] = useState('')
     const [isDoing, setIsDoing] = useState(false)
     const router = useRouter()
 
@@ -26,6 +27,7 @@ const CreateProductForm = (props: Props) => {
                 const data = res.data
                 if (data.status === 200) {
                     toast.success(data.message)
+                    setProductName('')
                     router.refresh()
                 } else {
                     toast.error(data.message)
@@ -51,6 +53,9 @@ const CreateProductForm = (props: Props) => {
             <div className='flex gap-2 items-center'>
                 <div>
                     <Input placeholder='New Product Name' className='h-10' value={productName} onChange={(e: any) => { setProductName(e.target.value) }} />
+                </div>
+                <div>
+                    <Input readOnly className='h-10 cursor-pointer' value={imageUrl? imageUrl : "Select Image"} />
                 </div>
                 <div>
                     <button onClick={() => createProduct()} className='text-sm bg-slate-100 active:bg-green-50 hover:bg-slate-100/30 p-2 rounded drop-shadow-sm border'>{isDoing ? "Please wait..." : "Add Product"}</button>
