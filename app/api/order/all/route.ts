50c3af612d588bcf14b4e7c3d1d041fad6fa7945
Baidar Gul <baidargul@outlook.com>
2023-12-11 12:44:29 +0500

    orders api are now date of booking asc

--- app/api/order/all/route.ts
+++ app/api/order/all/route.ts
@@ -42,7 +42,7 @@ export async function GET(req: NextRequest) {
                 },
             },
             orderBy: {
-                dateOfBooking: "desc",
+                dateOfBooking: "asc",
             }
         });
 
