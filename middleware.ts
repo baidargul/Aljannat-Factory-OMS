9217aef0ea244f94b174e41564ea733ca7c4c75a
Baidar Gul <baidargul@outlook.com>
2023-12-16 20:39:43 +0500

    app reset

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({});
  
 export const config = {
-  publicRoutes: ["/"],
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
  
