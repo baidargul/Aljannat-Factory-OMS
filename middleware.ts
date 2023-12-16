0ab40eff7aba972b96146e527aabb409915d84f7
Baidar Gul <baidargul@outlook.com>
2023-12-16 19:49:26 +0500

    changed middleware

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,6 @@ export default authMiddleware({});
  
 export const config = {
   publicRoutes: ["/"],
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/(api|trpc)(.*)'],
 };
  
