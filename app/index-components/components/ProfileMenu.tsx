f89caf3404909aca2a85588062ed3e3a57894694
Baidar Gul <baidargul@outlook.com>
2023-12-20 20:01:27 +0500

    Profile Menu Aniamtion

--- app/index-components/components/ProfileMenu.tsx
+++ app/index-components/components/ProfileMenu.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 const ProfileMenu = (props: Props) => {
     return (
-        <div className='flex items-center gap-2 bg-white p-1 rounded drop-shadow-sm'>
+        <div className='flex items-center gap-2 bg-white p-1 rounded drop-shadow-sm group cursor-pointer'>
             <div>
                 <Image src='/Placeholders/default.png' width={30} height={30} alt='userProfile' className='rounded-full' />
             </div>
@@ -18,7 +18,7 @@ const ProfileMenu = (props: Props) => {
                     baidargul@outlook.com
                 </div>
             </div>
-            <div className='text-slate-500'>
+            <div className='text-slate-500 group-hover:mt-1 transition-all duration-500'>
                 <ChevronDown size={20} />
             </div>
         </div>
