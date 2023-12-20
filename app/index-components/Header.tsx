d76c842615cd92bd35d66d5f82d4022fa3e5295b
Baidar Gul <baidargul@outlook.com>
2023-12-20 19:56:32 +0500

    Created Profile DropDown Menu Button on Header

--- app/index-components/Header.tsx
+++ app/index-components/Header.tsx
@@ -1,22 +1,29 @@
-import { Brackets, Menu } from 'lucide-react'
+import { ArrowDown, ChevronDown, Menu } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import ProfileMenu from './components/ProfileMenu'
 
 type Props = {}
 
 const Header = (props: Props) => {
-  return (
-    <div className='flex p-4 bg-slate-100 items-center gap-2 select-none text-slate-700'>
-        <div className='cursor-pointer hover:skew-x-12 transition-all duration-500'>
-            <Menu size={24} />
+    return (
+        <div className='flex p-4 bg-slate-100 items-center justify-between select-none text-slate-700'>
+            <div className='flex gap-2 items-center group'>
+                <div className='cursor-pointer hover:skew-x-12 transition-all duration-500'>
+                    <Menu size={24} />
+                </div>
+                <div className='text-2xl font-semibold font-sans group-hover:tracking-wide transition-all duration-500'>
+                    <Link href='/'>
+                        Aljannat
+                    </Link>
+                </div>
+            </div>
+            <div>
+                <ProfileMenu />
+            </div>
         </div>
-        <div className='text-2xl font-semibold font-sans'>
-            Aljannat
-        </div>
-        <div>
-
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Header
---
+++ app/index-components/components/ProfileMenu.tsx
@@ -0,0 +1,28 @@
+import { ChevronDown } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
+
+type Props = {}
+
+const ProfileMenu = (props: Props) => {
+    return (
+        <div className='flex items-center gap-2 bg-white p-1 rounded drop-shadow-sm'>
+            <div>
+                <Image src='/Placeholders/default.png' width={30} height={30} alt='userProfile' className='rounded-full' />
+            </div>
+            <div className='flex flex-col'>
+                <div className='font-bold'>
+                    Baidar Gul
+                </div>
+                <div className='text-sm -mt-1 scale-90 -ml-2'>
+                    baidargul@outlook.com
+                </div>
+            </div>
+            <div className='text-slate-500'>
+                <ChevronDown size={20} />
+            </div>
+        </div>
+    )
+}
+
+export default ProfileMenu
