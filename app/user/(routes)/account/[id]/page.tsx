29e9d52d0186f818003621010974ec7c138c0e15
Baidar Gul <baidargul@outlook.com>
2023-12-20 19:03:39 +0500

    updated profile page

--- app/user/(routes)/account/[id]/page.tsx
+++ app/user/(routes)/account/[id]/page.tsx
@@ -31,10 +31,6 @@ const UserAccount = async (props: Props) => {
 
     if (!user) redirect('404')
 
-    const settings = [
-        "Profile", "APIs", "Orders", "Activities"
-    ]
-
     return (
         <div className='select-none bg-gradient-to-b from-stone-50 to-slate-100 min-h-min w-full'>
             <div className='p-2'>
@@ -43,24 +39,24 @@ const UserAccount = async (props: Props) => {
                         <section className=''>
                             <div className=''>
                                 {
-                                    profile && (
-                                        <Image src={profile ? String(profile.imageURL) : "/profile/default.jpg"} width={300} height={300} className='rounded-full drop-shadow-md border-8 border-white' alt={profile && profile.name} />
+                                    user && (
+                                        <Image src={user ? String(user.imageURL) : "/profile/default.jpg"} width={300} height={300} className='rounded-full drop-shadow-md border-8 border-white' alt={user && user.name} />
                                     )
                                 }
                             </div>
                         </section>
                         <section>
                             <div className='text-4xl '>
-                                {profile?.name}
+                                {user?.name}
                             </div>
                             <div className='text-sm  bg-gradient-to-r from-rose-100 to-orange-200 w-fit px-2 rounded-md drop-shadow-sm uppercase'>
-                                {profile && getStage(profile?.role)}
+                                {user && getStage(user?.role)}
                             </div>
                             <div>
-                                {profile?.email}
+                                {user?.email}
                             </div>
                             <div className='text-xs uppercase'>
-                                {profile?.id}
+                                {user?.id}
                             </div>
                         </section>
                     </div>
@@ -72,30 +68,7 @@ const UserAccount = async (props: Props) => {
             <div className='p-2 h-[400px] w-full'>
                 <div className='w-full'>
                     <section className='p-2 bg-white rounded-t-md drop-shadow-sm w-full'>
-                        <div className='tracking-wide font-semibold text-lg text-black/70 text-center'>
-                            Settings
-                        </div>
-                        <div className='mt-2 w-full'>
-                            <div className='px-10'>
-                                <section className='flex gap-1 justify-between items-center'>
-                                    {
-                                        settings.map((setting: string) => (
-                                            <div key={v4()} className='group active:bg-orange-400 active:text-white rounded text-sm flex gap-1 items-center'>
-                                                <div className='p-1  group-hover:animate-pulse duration-1000 opacity-0 group-hover:opacity-70 transition-all'>
-                                                    <Scan className='w-4 h-4 group-active:text-white group-active:scale-110' />
-                                                </div>
-                                                <div className='p-1 group-hover:first-letter:text-red-800 group-active:first-letter:text-white group-active:text-white duration-1000 transition-all text-black/70'>
-                                                    {setting}
-                                                </div>
-                                            </div>
-                                        ))
-                                    }
-                                </section>
-                                <section>
-
-                                </section>
-                            </div>
-                        </div>
+                        
                     </section>
                     <section>
 
