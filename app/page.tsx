79428c84577bb39c9288757cad05ca42c78b3a35
Baidar Gul <baidargul@outlook.com>
2023-12-21 17:42:32 +0500

    Ui fixing

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   if (profile.role === Role.PAYMENTVERIFIER) redirect(`/orders/verify`)
 
   return (
-    <div className={`flex select-none gap-2 justify-center items-center min-h-screen p-4 cursor-default`}>
+    <div className={`flex select-none gap-2 justify-center items-center p-4 cursor-default`}>
       <div className="">
         HomePage
       </div>
