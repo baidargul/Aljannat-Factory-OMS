10ddf5efe750b491dbf88b7f3ed7c4eb8f916d02
Baidar Gul <baidargul@outlook.com>
2023-12-23 20:39:04 +0500

    ui fixing

--- app/user/(routes)/dashboard/components/GlobalMenu/SettingList/SettingList.tsx
+++ app/user/(routes)/dashboard/components/GlobalMenu/SettingList/SettingList.tsx
@@ -20,11 +20,11 @@ const SettingList = (props: Props) => {
         <table className='w-full'>
             <thead>
                 <tr>
-                    <th className='border text-start pl-2 bg-zinc-600 text-white tracking-wide'>Name</th>
-                    <th className='border text-start pl-2 bg-zinc-600 text-white tracking-wide'>Value 1</th>
-                    <th className='border text-start pl-2 bg-zinc-600 text-white tracking-wide'>Value 2</th>
-                    <th className='border text-start pl-2 bg-zinc-600 text-white tracking-wide'>Value 3</th>
-                    <th className='border text-end pr-4 bg-zinc-600 text-white tracking-wide'>Actions</th>
+                    <th className='border text-start pl-2 p-2 bg-zinc-600 text-white tracking-wide'>Name</th>
+                    <th className='border text-start pl-2 p-2 bg-zinc-600 text-white tracking-wide'>Value 1</th>
+                    <th className='border text-start pl-2 p-2 bg-zinc-600 text-white tracking-wide'>Value 2</th>
+                    <th className='border text-start pl-2 p-2 bg-zinc-600 text-white tracking-wide'>Value 3</th>
+                    <th className='border text-end pr-4 p-2 bg-zinc-600 text-white tracking-wide'>Actions</th>
                 </tr>
             </thead>
             <tbody>
