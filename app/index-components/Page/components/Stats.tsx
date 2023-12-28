0e2be7932576aed8d383d6cb17620049f5a2bbb5
Baidar Gul <baidargul@outlook.com>
2023-12-28 21:32:42 +0500

    Converted to client

--- app/index-components/Page/components/Stats.tsx
+++ app/index-components/Page/components/Stats.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useEffect, useState } from 'react';
 import OrderStats from './sub/OrderStats';
 import OrderNotes from '@/app/user/(routes)/dashboard/components/sub/OrderNotes';
