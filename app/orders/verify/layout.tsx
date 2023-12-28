f36b9c52e323c831123411413090868980acfa91
Baidar Gul <baidargul@outlook.com>
2023-12-28 21:07:08 +0500

    removed refresh rate container from orders/verify page

--- app/orders/verify/layout.tsx
+++ app/orders/verify/layout.tsx
@@ -21,9 +21,9 @@ const OrdersLayout = async (props: Props) => {
   return (
     <div>
       <div className='flex justify-between w-full p-2 min-h-screen select-none'>
-        <div className='w-[15%] bg-slate-100 rounded-md p-2'>
+        <div className='w-[15%] bg-slate-100 rounded-md p-2 flex flex-col gap-4'>
           <UserInformation profile={profile} />
-          <RefreshRateContainer />
+          {/* <RefreshRateContainer /> */}
           <OrdersSummary />
         </div>
         <div className='w-[85%]'>{props.children}</div>
