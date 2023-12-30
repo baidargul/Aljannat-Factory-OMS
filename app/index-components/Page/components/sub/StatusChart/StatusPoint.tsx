7019925f792abab61de9027139e12389bcc98143
Baidar Gul <baidargul@outlook.com>
2023-12-30 14:53:49 +0500

    UI fix

--- app/index-components/Page/components/sub/StatusChart/StatusPoint.tsx
+++ app/index-components/Page/components/sub/StatusChart/StatusPoint.tsx
@@ -6,17 +6,22 @@ type Props = {
 
 const StatusPoint = (props: Props) => {
     return (
-        <div className='bg-gradient-to-b from-slate-50 to-slate-100 p-10 w-40 h-32 rounded-md drop-shadow-sm text-center border-b border-slate-200'>
+        <div className={`bg-gradient-to-b from-slate-50 to-slate-100 p-10 w-40 h-32 rounded-md drop-shadow-sm text-center border-b border-slate-200`}>
             <div className='flex flex-col  justify-between items-center gap-2'>
                 <div className='text-xs font-semibold'>
                     {props.item.name}
                 </div>
                 <div className='flex gap-1 items-center bg-gradient-to-b from-slate-100 to-slate-200 border-b border-slate-200 p-1 rounded-md w-fit'>
-                    <div className='text-2xl'>
+                    <div className=''>
                         {props.item.inWork}
                     </div>
-                    <div>
-                        {`/${props.item.order}`}
+                    <div className='flex items-center text-2xl'>
+                        <div>
+                            /
+                        </div>
+                        <div>
+                            {`${props.item.order}`}
+                        </div>
                     </div>
                 </div>
             </div>
