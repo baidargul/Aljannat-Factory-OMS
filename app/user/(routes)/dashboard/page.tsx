6df4ca8ca254933d9104963cdfdfc5356bf2bf0b
Baidar Gul <baidargul@outlook.com>
2023-12-30 13:51:19 +0500

    added restriction to dashboard page

--- app/user/(routes)/dashboard/page.tsx
+++ app/user/(routes)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import Menu from './components/Menu'
 import currentProfile from '@/lib/current-profile'
 import { redirectToSignIn } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
+import { Role } from '@prisma/client'
 
 type Props = {}
 
@@ -12,13 +13,26 @@ const page = async (props: Props) => {
         redirect('/')
     }
 
-    return (
-        <div>
-            <div className='mt-20'>
-                <Menu profile={profile} />
+    if (profile.role === Role.ADMIN || profile.role === Role.SUPERADMIN) {
+        return (
+            <div>
+                <div className='mt-20'>
+                    <Menu profile={profile} />
+                </div>
             </div>
-        </div>
-    )
+        )
+
+    } else {
+
+        return(
+            <div>
+                <div className='flex justify-center items-center min-h-screen w-full select-none '>
+                    <h1 className='text-3xl font-bold h-fit bg-slate-100/50 p-1 rounded-md border-b-2 border-slate-100'>Your role is not allowed to view this page</h1>
+                </div>
+            </div>
+        )
+    }
+
 }
 
 export default page
