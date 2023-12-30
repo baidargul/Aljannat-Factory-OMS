ff800c5e47dedb8312423ba86cbdfad873adb23a
Baidar Gul <baidargul@outlook.com>
2023-12-30 13:47:27 +0500

    added restrictions to Create Product page, Create Order page

--- app/(create)/create/order/page.tsx
+++ app/(create)/create/order/page.tsx
@@ -12,6 +12,18 @@ const page = async (props: Props) => {
         redirectToSignIn()
     } else {
         if (profile.role === Role.UNVERIFIED) redirect(`/home/unverified/${profile.userId}`)
+        if (
+            profile.role !== Role.SUPERADMIN &&
+            profile.role !== Role.ADMIN &&
+            profile.role !== Role.ORDERBOOKER &&
+            profile.role !== Role.MANAGER
+        ) {
+            return (
+                <div className='flex justify-center items-center min-h-screen w-full select-none '>
+                    <h1 className='text-3xl font-bold h-fit bg-slate-100/50 p-1 rounded-md border-b-2 border-slate-100'>Your role is not allowed to view this page</h1>
+                </div>
+            )
+        }
     }
 
     return (
