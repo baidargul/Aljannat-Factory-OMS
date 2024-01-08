4db495e4960fcbd0f787e0d88e19120ca5d1b302
Baidar Gul <baidargul@outlook.com>
2024-01-08 13:34:08 +0500

    image disabled for product, name bug fixed

--- app/(create)/create/products/components/AvailableProducts.tsx
+++ app/(create)/create/products/components/AvailableProducts.tsx
@@ -7,6 +7,7 @@ import { ScrollArea } from '@/components/ui/scroll-area'
 import { PlusCircleIcon } from 'lucide-react'
 import DialogProvider from '@/components/DialogProvider/DialogProvider'
 import CreateProductForm from './CreateProductForm'
+import { formalizeText } from '@/lib/my'
 type Props = {}
 
 
@@ -33,16 +34,16 @@ const AvailableProducts = async (props: Props) => {
                             return (
                                 <div key={product.id}>
                                     <AvailableProductSelector product={product}>
-                                        <div className='flex gap-1 items-center group hover:drop-shadow-lg transition-all duration-1000 bg-gradient-to-r from-slate-50 to-slate-200 drop-shadow-sm border border-slate-200 hover:border-slate-300 p-2 rounded w-60'>
-                                            <div className='border border-white drop-shadow-sm group-hover: transition-all'>
+                                        <div className='flex h-20 gap-1 items-center group hover:drop-shadow-lg transition-all duration-1000 bg-gradient-to-r from-slate-50 to-slate-200 drop-shadow-sm border border-slate-200 hover:border-slate-300 p-2 rounded w-60'>
+                                            {/* <div className='border border-white drop-shadow-sm group-hover: transition-all'>
                                                 <Image src={product.imageUrl ? product.imageUrl : `/Placeholders/question.png`} className='w-16 h-16 rounded' width={100} height={100} alt='image' />
-                                            </div>
+                                            </div> */}
                                             <div className='flex flex-col items-start text-start'>
                                                 <div className='font-semibold tracking-tights'>
-                                                    {product.name}
+                                                    {formalizeText(product.name)}
                                                 </div>
                                                 <div className={`text-xs text-start w-fit transition-all duration-1000 -ml-2 scale-75 p-1 bg-slate-50 border border-slate-300 rounded-md ${productVariations.length < 1 && "hidden"}`}>
-                                                    <div className={`flex gap-1 items-start ${productVariations.length < 1 && "hiddesn"}`}>
+                                                    <div className={`flex gap-1 items-start ${productVariations.length < 1 && "hidden"}`}>
                                                         <div>
                                                             {productVariations.length ? productVariations.length : 0}
                                                         </div>
