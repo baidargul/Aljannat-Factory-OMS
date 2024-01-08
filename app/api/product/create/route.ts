5dd2c49597f7c8a02c7806d7f1211b319f1bb1d4
Baidar Gul <baidargul@outlook.com>
2024-01-08 13:28:55 +0500

    removed image from product create route

--- app/api/product/create/route.ts
+++ app/api/product/create/route.ts
@@ -28,12 +28,12 @@ export async function POST(req: NextRequest) {
             return new Response(JSON.stringify(response))
         }
 
-        if (!image) {
-            response.status = 400
-            response.message = "Image is required."
-            response.data = null
-            return new Response(JSON.stringify(response))
-        }
+        // if (!image) {
+        //     response.status = 400
+        //     response.message = "Image is required."
+        //     response.data = null
+        //     return new Response(JSON.stringify(response))
+        // }
 
         const isAlreadyExists = await prisma.product.findUnique({
             where: {
@@ -49,35 +49,35 @@ export async function POST(req: NextRequest) {
         }
 
         // Decode base64 image data
-        const decodedImageData = Buffer.from(image.split(",")[1], "base64");
+        // const decodedImageData = Buffer.from(image.split(",")[1], "base64");
 
         // AWS configuration
-        const s3 = new AWS.S3({
-            accessKeyId: "AKIATIQPEIGEHWZIEOP3",
-            secretAccessKey: "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",
-            region: "ap-south-1",
-        });
+        // const s3 = new AWS.S3({
+        //     accessKeyId: "AKIATIQPEIGEHWZIEOP3",
+        //     secretAccessKey: "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",
+        //     region: "ap-south-1",
+        // });
 
         // S3 parameters
-        const fileContent = decodedImageData;
-        const fileType = String(name).toLocaleLowerCase() + `.` + fileExtension(fileName);
-        const params = {
-            Bucket: "aljannat/orderPortal/products",
-            Key: fileType,
-            Body: fileContent,
-        };
+        // const fileContent = decodedImageData;
+        // const fileType = String(name).toLocaleLowerCase() + `.` + fileExtension(fileName);
+        // const params = {
+        //     Bucket: "aljannat/orderPortal/products",
+        //     Key: fileType,
+        //     Body: fileContent,
+        // };
 
         // Upload to S3
-        const result = await s3.upload(params).promise();
+        // const result = await s3.upload(params).promise();
         // Save to database
-        let fileUrl
-        fileUrl = result.Location;
-        const base64FileContent = fileContent.toString("base64");
+        // let fileUrl
+        // fileUrl = result.Location;
+        // const base64FileContent = fileContent.toString("base64");
 
         const newProduct = await prisma.product.create({
             data: {
                 name: String(name).toLocaleLowerCase(),
-                imageUrl: fileUrl
+                imageUrl: null//fileUrl
             }
         })
 
